5cc07c21a7a8ab2383a003618682572e9ac7ebfe
vagnero <vagnerimperador16@gmail.com>
2024-10-17 22:53:28 -0300

    Atualizacao na url

--- src/main/java/com/talkconnect/configs/WebSocketConfig.java
+++ src/main/java/com/talkconnect/configs/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://10.0.0.190", "https://talkinvagner.shop").withSockJS();
+        registry.addEndpoint("/api/ws").setAllowedOrigins("http://localhost:3000", "http://10.0.0.190", "https://talkinvagner.shop").withSockJS();
     }
 }
