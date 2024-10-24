d149858214a456670ec104cf82162ba3b279c122
vagnero <vagnerimperador16@gmail.com>
2024-10-24 19:21:08 -0300

    atualizacao no controller ami

--- src/main/java/com/talkconnect/controllers/ami/AsteriskAmiController.java
+++ src/main/java/com/talkconnect/controllers/ami/AsteriskAmiController.java
@@ -1,22 +1,33 @@
 package com.talkconnect.controllers.ami;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talkconnect.services.ami.AsteriskAmiService;
-
 @RestController
 @RequestMapping("/api/ami")
 public class AsteriskAmiController {
     @Autowired
     private AsteriskAmiService asteriskAMIService;
 
-    @GetMapping("/command")
-    public String runCommand(@RequestParam String command) {
-        return asteriskAMIService.sendCommand(command);
+    @PostMapping("/command")
+    public String runCommand(@RequestBody CommandRequest commandRequest) {
+        return asteriskAMIService.sendCommand(commandRequest.getCommand());
     }
     
+    public static class CommandRequest {
+        private String command;
+
+        // Getter e Setter
+        public String getCommand() {
+            return command;
+        }
+
+        public void setCommand(String command) {
+            this.command = command;
+        }
+    }
 }
