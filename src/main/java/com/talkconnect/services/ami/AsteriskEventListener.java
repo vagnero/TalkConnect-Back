b3ac858291db0db26f823b0ca1e5b2922c050da3
vagnero <vagnerimperador16@gmail.com>
2024-10-24 20:31:45 -0300

    atualizacao nos eventos

--- src/main/java/com/talkconnect/services/ami/AsteriskEventListener.java
+++ src/main/java/com/talkconnect/services/ami/AsteriskEventListener.java
@@ -14,29 +14,6 @@ public class AsteriskEventListener implements ManagerEventListener {
 
     @Override
     public void onManagerEvent(ManagerEvent event) {
-        if (event instanceof NewChannelEvent) {
-            NewChannelEvent newChannelEvent = (NewChannelEvent) event;
-            System.out.println("Novo canal criado: " +
-                newChannelEvent.getChannel() + " - Origem: " + newChannelEvent.getCallerIdNum());
-        } else if (event instanceof HangupEvent) {
-            HangupEvent hangupEvent = (HangupEvent) event;
-            System.out.println("Chamada encerrada: " +
-                hangupEvent.getCallerIdNum() + " - Canal: " + hangupEvent.getChannel());
-        } else if (event instanceof DialEvent) {
-            DialEvent dialEvent = (DialEvent) event;
-            System.out.println("Ligação de: " +
-                dialEvent.getCallerIdNum() + " para: " + dialEvent.getDestination());
-        } else if (event instanceof ConfbridgeJoinEvent) {
-            ConfbridgeJoinEvent confbridgeJoinEvent = (ConfbridgeJoinEvent) event;
-            System.out.println("Participante entrou na conferência: " +
-                confbridgeJoinEvent.getCallerIdNum() + " - Conferência: " + confbridgeJoinEvent.getConference());
-        } else if (event instanceof ConfbridgeLeaveEvent) {
-            ConfbridgeLeaveEvent confbridgeLeaveEvent = (ConfbridgeLeaveEvent) event;
-            System.out.println("Participante saiu da conferência: " +
-                confbridgeLeaveEvent.getCallerIdNum() + " - Conferência: " + confbridgeLeaveEvent.getConference());
-        } else {
-            // Para outros eventos, você pode logar ou ignorar
-            System.out.println("Evento recebido: " + event.getClass().getSimpleName());
-        }
+        System.out.println("Event received: " + event);
     }
 }
